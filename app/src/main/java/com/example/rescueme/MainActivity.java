664942a84b91fdc5b6805ac1793d3de2af8de5a2
DanTheBeginner09 <148887741+DanTheBeginner09@users.noreply.github.com>
2024-03-12 01:22:03 +0800

    First Commit

---
+++ app/src/main/java/com/example/rescueme/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.rescueme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView RegisterRedirection;
+    CheckBox myCheckBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // method to color red and redirection to register
+        RegisterTORedColor();
+        // remember method
+        checkRemember();
+
+
+
+    }
+
+    public void RegisterTORedColor(){
+        RegisterRedirection=findViewById(R.id.RegisterRedirection);
+
+        String ToColorRed=RegisterRedirection.getText().toString();
+        String targetWord="Register";
+        int startIndex = ToColorRed.indexOf(targetWord);
+        SpannableString spannableString = new SpannableString(ToColorRed);
+        spannableString.setSpan(new ForegroundColorSpan(Color.RED), startIndex, startIndex + targetWord.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        RegisterRedirection.setText(spannableString);
+
+    }
+
+    public void checkRemember(){
+        CheckBox myCheckBox = findViewById(R.id.myCheckBox);
+
+        myCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Check the state of the CheckBox
+                boolean isChecked = ((CheckBox) v).isChecked();
+
+                // Show a toast message based on the CheckBox state
+                if (isChecked) {
+                    Toast.makeText(MainActivity.this, "Checkbox is checked", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Checkbox is unchecked", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/rescueme/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.rescueme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long SPLASH_DELAY = 4000; // 4 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Create an Intent to start the main activity
+                Intent Intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(Intent);
+                finish(); // Close the splash activity so it's not accessible after the main activity starts
+            }
+        }, SPLASH_DELAY);
+    }
+}
