764f8b200f164af3c3984a5840d3992cc29d2912
DanTheBeginner09 <148887741+DanTheBeginner09@users.noreply.github.com>
2024-03-14 12:58:30 +0800

    Ika upat na commit n Daniel

--- app/src/main/java/com/example/rescueme/RegisterActivity.java
+++ app/src/main/java/com/example/rescueme/RegisterActivity.java
@@ -11,6 +11,7 @@ import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,33 +21,62 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
+
+
     TextView EVLogin;
-    Button BregisterSave_button;
+    Button BregisterSave_button, Bregister_reset;
+    TextView TVregister_selectBarangay;
 
     EditText ETregister_username, ETregister_email,ETregister_password, ETregister_confirmedpassword, ETregister_realName;
+
+
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
        //condition method
         RedLoginToRegister();
 
     BregisterSave_button=findViewById(R.id.registerSave_button);
+        Bregister_reset=findViewById(R.id.register_reset);
     ETregister_username=findViewById(R.id.register_username);
     ETregister_email=findViewById(R.id.register_email);
     ETregister_password=findViewById(R.id.register_password);
     ETregister_confirmedpassword=findViewById(R.id.register_confirmedpassword);
     ETregister_realName=findViewById(R.id.register_realName);
 
+
     //Method
         BregisterSave_button.setOnClickListener(v-> CreateAccountBtn());
+        Bregister_reset.setOnClickListener(v-> ResetBtn());
 
 
 
     }//end of oncreate
 
+
+    //Sarting of method function
+
+
+//unresolve problem pani
+    public  void  barangaySelection(){
+
+        TVregister_selectBarangay=findViewById(R.id.register_selectBarangay);
+        String[] fruits = {"Apple", "Banana", "Cherry", "Date", "Fig", "Grape", "Kiwi", "Lemon", "Mango", "Orange", "Peach", "Pear"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, fruits);
+
+
+    } // END OF METHOD
+
+
+
+    // Start method
 
    public void CreateAccountBtn(){
 
@@ -65,6 +95,24 @@ public class RegisterActivity extends AppCompatActivity {
            BregisterSave_button.setOnClickListener(new View.OnClickListener() {
                @Override
                public void onClick(View v) {
+
+                   database = FirebaseDatabase.getInstance();
+                   reference= database.getReference("enrolled");
+
+
+                   String username= ETregister_username.getText().toString();
+                   String realName= ETregister_realName.getText().toString();
+                   String email=ETregister_email.getText().toString();
+                   String password=ETregister_password.getText().toString();
+
+                   HelperClass helperClass = new HelperClass( username, realName, email, password);
+                   reference.child(username).setValue(helperClass);
+
+
+                 /*  FirebaseDatabase database = FirebaseDatabase.getInstance();
+                   DatabaseReference myRef = database.getReference("test user3");
+
+                   myRef.setValue("Successfully");*/
 
                    Toast.makeText( RegisterActivity.this,"You have successfully register!", Toast.LENGTH_SHORT).show();
 
@@ -113,8 +161,24 @@ public class RegisterActivity extends AppCompatActivity {
         }
 
         return true;
-    }
+    }// end of validate data
 
+
+    public void ResetBtn(){
+
+
+            Bregister_reset.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ETregister_username.setText("");
+                    ETregister_email.setText("");
+                    ETregister_password.setText("");
+                    ETregister_confirmedpassword.setText("");
+                    ETregister_realName.setText("");
+                }
+            });
+
+    }
 
 
     //Sa RegisterActivity balhin to MainActivity
